2d08691ed8adfa75d18f58ed91cc6e8594e12e32
Oleh Makarytskyi <makarytskyi.o@ajax.systems>
2024-12-10 11:13:42 +0200

    changed gha

--- buildSrc/src/main/kotlin/com/makarytskyi/MyPlugin.kt
+++ buildSrc/src/main/kotlin/com/makarytskyi/MyPlugin.kt
@@ -1,6 +1,5 @@
 package com.makarytskyi
 
-import java.io.File
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -8,21 +7,55 @@ import org.gradle.api.tasks.TaskAction
 
 class MyPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.tasks.register("myTask", MyTask::class.java)
+        project.tasks.register("myTask", CreatePullRequestTask::class.java)
     }
 }
 
-open class MyTask : DefaultTask() {
+open class CreatePullRequestTask : DefaultTask() {
+
+    private val branchName = "feature/pr-branch-${System.currentTimeMillis()}"
+    private val targetBranch = "main"
+    private val githubToken = System.getenv("GITHUB_TOKEN")
+
     @TaskAction
-    fun createFile() {
-        val file = File(project.projectDir, "abc.txt")
+    fun createPullRequest() {
+        executeCommand("git checkout -b $branchName")
+        executeGradleBuild()
+        commitChanges()
+        pushChanges()
+        createPullRequestOnGitHub()
+    }
 
-        if (file.createNewFile()) {
-            println("File abc.txt created at: ${file.absolutePath}")
-        } else {
-            println("File abc.txt already exists at: ${file.absolutePath}")
-        }
+    private fun executeCommand(command: String) {
+        val process = ProcessBuilder(command.split(" ")).start()
+        val output = process.inputStream.bufferedReader().readText()
+        println(output)
+    }
 
-        file.writeText("This is a sample text inside abc.txt file.")
+    private fun executeGradleBuild() {
+        val process = ProcessBuilder("./gradlew", "build", "-parallel").start()
+        val output = process.inputStream.bufferedReader().readText()
+        println(output)
+    }
+
+    private fun commitChanges() {
+        println("Committing changes...")
+        executeCommand("git config --global user.email 'github-actions@github.com'")
+        executeCommand("git config --global user.name 'github-actions'")
+        executeCommand("git add .")
+        executeCommand("git commit -m 'Add files'")
+    }
+
+    private fun pushChanges() {
+        println("Pushing changes to GitHub...")
+        val gitPushCommand = "git push -u origin $branchName"
+        executeCommand(gitPushCommand)
+    }
+
+    private fun createPullRequestOnGitHub() {
+        println("Creating pull request from $branchName to $targetBranch...")
+        val process = ProcessBuilder("gh", "pr", "create", "--head", branchName, "--base", targetBranch, "--title", "Automated PR", "--body", "This PR is automatically created via Gradle plugin.").start()
+        val output = process.inputStream.bufferedReader().readText()
+        println(output)
     }
 }
